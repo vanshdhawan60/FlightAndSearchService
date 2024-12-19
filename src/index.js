1b099f9dde300a36578703a110a1dace9b9475b4
vanshdhawan60 <116313452+vanshdhawan60@users.noreply.github.com>
2024-12-19 06:30:24 +0530

    Added express.json parsing etc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const {PORT} = require('./config/serverConfig')
 
 const setupAndStartServer = async () => {
     const app = express();
+    app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
     app.listen(PORT, () => {
         console.log(`started server on port ${PORT}`);
     })
