626c26e00f637dbc6f3a0053ab27c04a0dd20c0f
vanshdhawan60 <116313452+vanshdhawan60@users.noreply.github.com>
2024-12-25 20:22:41 +0530

    added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,42 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+    constructor() {
+        this.cityRepo = new CityRepository();
+    }
+    async createCity(data) {
+        try {
+            const city = await this.cityRepo.createCity(data);
+            return city;
+        } catch (error) {
+            console.log("Somethign went wrong in the city-service layer.");
+            throw({error});
+        }
+    }
+    async updateCity(cityId, data) {
+        try {
+            const city = await this.cityRepo.updateCity(cityId, data);
+            return city;
+        } catch (error) {
+            console.log("Somethign went wrong in the city-service layer.");
+            throw({error});
+        }
+    }
+    async deleteCity(cityId) {
+        try {
+            return await this.cityRepo.deleteCity(cityId);
+        } catch (error) {
+            console.log("Somethign went wrong in the city-service layer.");
+            throw({error});
+        }
+    }
+    async getCity(cityId) {
+        try {
+            const city = await this.cityRepo.getCity(cityId);
+            return city;
+        } catch (error) {
+            console.log("Somethign went wrong in the city-service layer.");
+            throw({error});
+        }
+    }
+}
